fc5489a4541cbb2245bc4558db3054fedb78ac9f
Artem Varabey <captainhorsepower228@gmail.com>
2019-09-10 14:46:36 +0300

    implement timetable download

---
+++ lib/back/data_parsing/timetable_downloader.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:path_provider/path_provider.dart';
+
+class TimetableDonwloader {
+  static const String thirdYearFAMCSScheldueUrl = 'http://fpmi.bsu.by/sm_full.aspx?guid=58093';
+
+  static ValueNotifier<String> lastDownloadedFilePath = ValueNotifier<String>('nothing downloaded yet');
+
+  static Future<void> download({void Function(String, DownloadTaskStatus, int) callback}) async {
+    // get path to donwloads directory
+    final downloadsDirectory = await getApplicationDocumentsDirectory();
+
+    // enqueue download task, that will be executed when possible
+    final taskId = await FlutterDownloader.enqueue(
+      url: '${TimetableDonwloader.thirdYearFAMCSScheldueUrl}',
+      savedDir: '${downloadsDirectory.path}',
+      showNotification: true, // show download progress in status bar (for Android)
+      openFileFromNotification: true, // click on notification to open downloaded file (for Android)
+    );
+
+    FlutterDownloader.registerCallback(
+      (id, status, progress) async {
+        // register callback, if provided (to reflect donwload progress in ui)
+        if (id == taskId && callback != null) {
+          callback(id, status, progress);
+        }
+
+        // when download is complete, update download file path
+        if (progress == 100) {
+          final tasks = await FlutterDownloader.loadTasks();
+          final lastTask = tasks.last;
+
+          final filePath = '${lastTask.savedDir.replaceAll('(null)', '')}${lastTask.filename}';
+          lastDownloadedFilePath.value = filePath;
+        }
+      },
+    );
+  }
+}
