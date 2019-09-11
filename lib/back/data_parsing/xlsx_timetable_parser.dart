7924a53f60024c2b9623dc9fc96d8e64afcebe82
Artem Varabey <captainhorsepower228@gmail.com>
2019-09-11 15:43:24 +0300

    do speciality parsing

--- lib/back/data_parsing/xlsx_timetable_parser.dart
+++ lib/back/data_parsing/xlsx_timetable_parser.dart
@@ -1,6 +1,19 @@
+import 'dart:collection';
 import 'dart:io';
 
 import 'package:spreadsheet_decoder/spreadsheet_decoder.dart';
+
+class _Pair {
+  final int left;
+  final int right;
+
+  _Pair(this.left, this.right);
+
+  @override
+  String toString() {
+    return 'left: $left, right: $right';
+  }
+}
 
 class XlsxTimetableParser {
   XlsxTimetableParser.fromFile(String pathToFile) {
@@ -29,26 +42,53 @@ class XlsxTimetableParser {
   int fridayRow;
   int saturdayRow;
 
+  Map<String, _Pair> _specialitiesMapInternal = HashMap();
+
   List<String> getSpecialties() {
-    List<String> specialities = List<String>();
+    List<String> specialitiesList = List<String>();
 
     // find row where specialities are listed
     for (int row = 0; row < table.maxRows; row++) {
+      Queue helper = Queue();
+
       for (int col = 0; col < table.maxCols; col++) {
         String value = table.rows[row][col]?.toString() ?? null;
+
         if (value == null) continue;
 
         if (value.contains('поток')) {
-          value = value
-              .replaceAll(RegExp(r'.*поток'), '').trim()  // remove 'n поток' префикс
-              .replaceAll(RegExp(r"\s{2,}"), ', ').trim();
-          specialities.add(value);
+          var speciality = value
+              .replaceFirst(RegExp(r'.*поток'), '')
+              .trim() // remove 'n поток' префикс
+              .replaceAll(RegExp(r"\s{2,}"), ', ')
+              .trim(); // remove stupid milliard of spaces in multiline headers
+
+          // queue contains data: l1, val1, r1 = l2, val2, r2 = l3, val3 ...
+          helper.addLast(col);
+          helper.addLast(speciality);
         }
       }
-      if (specialities.isNotEmpty) break;
+
+      // I assume that all specialities fit in one row, according to structure of the timetable
+      // so if I already found row with them, I can stop.
+      if (helper.isNotEmpty) {
+        while (helper.isNotEmpty) {
+          final int left = helper.removeFirst();
+          final String value = helper.removeFirst();
+          final int right = helper.isEmpty ? table.maxCols : helper.first;
+
+          final specs = value.split(', ');
+          specs.forEach(
+              (speciality) {
+                specialitiesList.add(speciality);
+                _specialitiesMapInternal[speciality] = _Pair(left, right);
+              });
+        }
+      }
     }
 
-    return specialities;
+    print(_specialitiesMapInternal.toString());
+    return specialitiesList;
   }
 
   void _setWeekDayRows() {
