0729810c1688c522586942a8bd8317f2d3b74c99
Artsiom Varabei <captainhorsepower228@gmail.com>
2020-09-26 22:40:00 +0300

    parse integer part

--- src/main/java/edu/varabei/artsiom/calculator/brain/DecimalParser.java
+++ src/main/java/edu/varabei/artsiom/calculator/brain/DecimalParser.java
@@ -1,8 +1,12 @@
 package edu.varabei.artsiom.calculator.brain;
 
+import lombok.Value;
+import lombok.val;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.stream.Stream;
 
 @Component
 public class DecimalParser {
@@ -11,6 +15,40 @@ public class DecimalParser {
         input = input.replaceAll("[\\s_]", "");
         input = input.replaceAll(",", ".");
         return new BigDecimal(input);
+    }
+
+    public String parse(BigDecimal decimal) {
+        String str = decimal.toString();
+        str = str.replaceAll("\\.?0+$", "");
+
+        BigDecimal scale = new BigDecimal(1000);
+        BigDecimal scaleStep = new BigDecimal(1000);
+
+//        decimal.divideToIntegralValue(BigDecimal.ONE)
+//                .divideAndRemainder()
+
+
+        return str;
+    }
+
+    public String formattedIntegerPart(BigDecimal decimal) {
+        StringBuilder result = new StringBuilder();
+        BigInteger intPart = decimal.toBigInteger();
+        while (intPart.compareTo(BigInteger.ZERO) > 0) {
+            final BigInteger[] divideAndRemainder = intPart.divideAndRemainder(BigInteger.valueOf(1000));
+            intPart = divideAndRemainder[0];
+            BigInteger remainder = divideAndRemainder[1];
+            result.insert(0, String.format("%03d ", divideAndRemainder[1].intValue()));
+        }
+        return result.toString().trim()
+                .replaceAll("^0+", "")
+                .replaceAll("^$", "0");
+    }
+
+    @Value
+    static class Pair<S, T> {
+        S left;
+        S right;
     }
 
 }
--- src/test/java/edu/varabei/artsiom/calculator/brain/DecimalParserTest.java
+++ src/test/java/edu/varabei/artsiom/calculator/brain/DecimalParserTest.java
@@ -3,6 +3,8 @@ package edu.varabei.artsiom.calculator.brain;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,6 +28,28 @@ class DecimalParserTest {
     public void bothDotAndComma() {
         assertTrue(decimalsEqual("1.1", parser.parse("1.1")));
         assertTrue(decimalsEqual("1.1", parser.parse("1,1")));
+    }
+
+    @Test
+    public void removeTrailingZeros() {
+        assertEquals("1", parser.parse(new BigDecimal("1.00000")));
+        assertEquals("1.01", parser.parse(new BigDecimal("1.01000")));
+    }
+
+    @Test
+    public void formattedIntegerPart() {
+        assertEquals("1 000", parser.formattedIntegerPart(
+                parser.parse("1_000")));
+        assertEquals("1 234 567 000", parser.formattedIntegerPart(
+                parser.parse("1_234_567_000")));
+        assertEquals("0", parser.formattedIntegerPart(
+                parser.parse("0000000")));
+    }
+
+    @Test
+    public void test() {
+        Arrays.stream(new BigDecimal("12345.101").toBigInteger().divideAndRemainder(new BigInteger("10")))
+                .forEach(System.out::println);
     }
 
     private boolean decimalsEqual(String expected, String actual) {
