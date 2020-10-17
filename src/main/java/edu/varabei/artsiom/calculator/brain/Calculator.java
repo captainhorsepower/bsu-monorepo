9a3e75e4f10e2c9435ae25af1a6adb62507e5dda
Artsiom Varabei <captainhorsepower228@gmail.com>
2020-10-17 21:02:23 +0300

    WIP: learn to use MathContext for rounding

--- src/main/java/edu/varabei/artsiom/calculator/brain/Calculator.java
+++ src/main/java/edu/varabei/artsiom/calculator/brain/Calculator.java
@@ -1,15 +1,22 @@
 package edu.varabei.artsiom.calculator.brain;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
 import java.math.RoundingMode;
+import java.util.function.Supplier;
 
 @Component
 @RequiredArgsConstructor
 public class Calculator {
 
     private final DecimalParser parser;
+    private final int resultScale;
+    private final int calcScale;
 
     public String add(String left, String right) {
         return parser.parse(
@@ -36,6 +43,40 @@ public class Calculator {
         return parser.parse(
                 parser.parse(left)
                         .divide(parser.parse(right), 6, RoundingMode.HALF_EVEN));
+    }
+
+//    public String calc(Expr expr, int roundingMode) {
+//        // have four numbers, three operations.
+//        // two middle numbers are immediately combined.
+//
+//        // then need to combine left with middle or middle with third.
+//        // and then the rest.
+//
+//        BigDecimal left, mid, right;
+//        mid = parser.parse(expr.midFirst.get())
+//                sop
+//            parser.parse(expr.midLast.get());
+//
+//        int op
+//        if (expr.top > expr.fop) {
+//
+//        }
+//        "subtract, add, multiply, divide";
+//        new MathContext()
+//        left.divi
+//        BigDecimal.class.getMethod()
+//
+//        BigDecimal result = left op right;
+//
+//        return parser.parse(result.setScale(resultScale, roundingMode));
+//    }
+
+
+    @Getter
+    public static class Expr {
+        public static final int ADD = 1, SUB = 1, MULT = 2, DIV = 2;
+        Supplier<String> first, midFirst, midLast, last;
+        Supplier<Integer> fop, sop, top; // first, second, third operation
     }
 
 }
--- src/test/java/edu/varabei/artsiom/calculator/brain/CalculatorTest.java
+++ src/test/java/edu/varabei/artsiom/calculator/brain/CalculatorTest.java
@@ -1,15 +1,18 @@
 package edu.varabei.artsiom.calculator.brain;
 
+import lombok.val;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest implements CompareDecimalsMixin {
 
     DecimalParser parser = new DecimalParser();
-    Calculator calc = new Calculator(parser);
+    Calculator calc = new Calculator(parser, 6, 10);
 
     @Test
     public void addSimpleCases() {
@@ -41,6 +44,33 @@ class CalculatorTest implements CompareDecimalsMixin {
         assertTrue(decimalsEqual(
                 "999_999_999_998",
                 calc.sub("1_000_000_000_000", "2.0000000000000")));
+    }
+
+    @Test
+    public void whatsMathContext() {
+        val roundUp = new MathContext(2, RoundingMode.UP);
+        val roundDown = new MathContext(2, RoundingMode.DOWN);
+        val roundHalfEven = new MathContext(2, RoundingMode.HALF_EVEN);
+        assertTrue(decimalsEqual(
+                "1.13",
+                BigDecimal.ONE
+                        .add(new BigDecimal(".123", roundUp))
+        ));
+        assertTrue(decimalsEqual(
+                "1.12",
+                BigDecimal.ONE
+                        .add(new BigDecimal(".123", roundDown))
+        ));
+        assertTrue(decimalsEqual(
+                "1.12",
+                BigDecimal.ONE
+                        .add(new BigDecimal(".125", roundHalfEven))
+        ));
+        assertTrue(decimalsEqual(
+                "1.12",
+                BigDecimal.ONE
+                        .add(new BigDecimal(".115", roundHalfEven))
+        ));
     }
 
 }
