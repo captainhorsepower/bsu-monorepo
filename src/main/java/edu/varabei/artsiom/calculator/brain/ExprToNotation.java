492ec3aa0cb12ef605ebf4e0e0fa9df5087d4214
Artsiom Varabei <captainhorsepower228@gmail.com>
2020-10-20 23:31:11 +0300

    can create postfix notation

---
+++ src/main/java/edu/varabei/artsiom/calculator/brain/ExprToNotation.java
@@ -0,0 +1,42 @@
+package edu.varabei.artsiom.calculator.brain;
+
+import lombok.val;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.function.BiPredicate;
+
+public class ExprToNotation {
+
+    public String toPostfixNotation(String expr) {
+        val math = "()+-*/";
+        BiPredicate<String, String> hasHigherPriority = (op1, op2) -> {
+            if (op2.equals("(")) return false;
+            val ind1 = math.indexOf(op1) / 2;
+            val ind2 = math.indexOf(op2) / 2;
+            return ind1 >= ind2;
+        };
+
+        Deque<String> stack = new ArrayDeque<>();
+        Deque<String> result = new ArrayDeque<>();
+        for (String item : expr.split(" ")) {
+            if (!math.contains(item))
+                result.add(item);
+            else if (stack.isEmpty())
+                stack.addFirst(item);
+            else {
+                if (hasHigherPriority.test(stack.getFirst(), item))
+                    while (!stack.isEmpty()) {
+                        val popped = stack.pop();
+                        if (!popped.equals("("))
+                            result.add(popped);
+                    }
+                if (!item.equals(")"))
+                    stack.addFirst(item);
+            }
+        }
+        while (!stack.isEmpty())
+            result.add(stack.pop());
+        return String.join(" ", result);
+    }
+}
---
+++ src/test/java/edu/varabei/artsiom/calculator/brain/ExprToNotationTest.java
@@ -0,0 +1,33 @@
+package edu.varabei.artsiom.calculator.brain;
+
+import lombok.val;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.function.BiPredicate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExprToNotationTest {
+
+    private ExprToNotation parser = new ExprToNotation();
+
+    @Test
+    public void stringTests() {
+        val expr = "a + b * c - d";
+        assertEquals("a b c * + d -", toPostfixNotation(expr));
+        assertEquals("a b +", toPostfixNotation("a + b"));
+        assertEquals("a b + c -", toPostfixNotation("a + b - c"));
+        assertEquals("a b c - +", toPostfixNotation("a + ( b - c )"));
+        assertEquals("a b c - +", toPostfixNotation("a + ( b - c )"));
+        assertEquals("a b c - *", toPostfixNotation("a * ( b - c )"));
+        assertEquals("a b c - * d /", toPostfixNotation("a * ( b - c ) / d"));
+        assertEquals("1.123 4.123 +", toPostfixNotation("1.123 + 4.123"));
+    }
+
+    String toPostfixNotation(String expr) {
+        return parser.toPostfixNotation(expr);
+    }
+
+}
