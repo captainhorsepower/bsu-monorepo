f016b17b5bef814289240161fd69fc40f36b1ed8
Artsiom Varabei <captainhorsepower228@gmail.com>
2020-12-04 14:27:48 +0300

    learn to read yaml

---
+++ December-2020/intellect/akinator/learn_python/parse_yaml.py
@@ -0,0 +1,7 @@
+import yaml
+
+a_yaml_file = open("example.yaml")
+parsed_yaml_file = yaml.load(a_yaml_file, Loader=yaml.FullLoader)
+
+print(f'dict: \n{parsed_yaml_file["a_dictionary"]}')
+print(f'list: \n{parsed_yaml_file["a_list"]}')
