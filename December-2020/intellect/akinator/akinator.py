f25dd1bab2f973477d23fe748b3c2562de4eb42f
Artsiom Varabei <captainhorsepower228@gmail.com>
2020-12-07 01:12:47 +0300

    refactor ascii

--- December-2020/intellect/akinator/akinator.py
+++ December-2020/intellect/akinator/akinator.py
@@ -5,15 +5,19 @@ import yaml  # PyYaml docs: https://pyyaml.org/wiki/PyYAMLDocumentation
 import random
 from glob import glob as findFiles
 
+
 def main():
     filename = chooseDatabaseFile()
     rules, target = chooseRules(filename)
     target, targetOptions = chooseTarget(rules, default=target)
 
     akinator = Akinator(rules, target)
-    akinator.resolveAns()
+    ansDict = akinator.solveAkinator()
 
-    drawResult(target, ansDict, fallbackOpts=targetOptions + ['Просто троллю'])
+    AsciiUtil.drawResult(target,
+                         ansDict,
+                         fallbackOpts=targetOptions + ['Просто троллю'],
+                         asked=akinator.getAsked(), guessed=akinator.getGuessed())
 
 
 def chooseDatabaseFile():
@@ -32,18 +36,18 @@ def chooseDatabaseFile():
 
     d = {_fileOptStr(f): f for f in dotYamls if (_fileOptStr(f))}
     костыль = 'Выход.'
-    c = ask(question="Выберите базу:",
-            options=list(d.keys()) + [костыль],
-            preHooks=[_drawMessageHook(
-                """
+    c = AsciiUtil.ask(question="Выберите базу:",
+                      options=list(d.keys()) + [костыль],
+                      preHooks=[AsciiUtil.drawMessageHook(
+                          """
 Добро пожаловать в ASCII акинатор.
 
 Сделал Воробей Артём, 4гр.
 """
-            )],
-            postHooks=[_drawMessageHook(f"\nОбнаруженные **.*.yaml: {dotYamls}\n"),
-                       _drawMessageHook(
-                """
+                      )],
+                      postHooks=[AsciiUtil.drawMessageHook(f"\nОбнаруженные **.*.yaml: {dotYamls}\n"),
+                                 AsciiUtil.drawMessageHook(
+                          """
 База знаний хранится в .yaml файлах со структурой:
 ---
 name: <имя базы>
@@ -101,7 +105,7 @@ def chooseTarget(rules, default):
         return f"{prefix:10}{k:15}{formatLongList(_targetOptions(k))}"
 
     invOpts = {_optStr(k): k for k in thenDict}
-    target = invOpts[ask(
+    target = invOpts[AsciiUtil.ask(
         'Что угадываем? Справа список выводимых правилами ответов', list(invOpts.keys()))]
     return target, _targetOptions(target)
 
@@ -109,15 +113,18 @@ def chooseTarget(rules, default):
 class Akinator:
 
     def __init__(self, rules, target) -> None:
-        self.targetRules = [r for r in self.rules if r.canAnswer(target)]
-        self.rules = [r for r in self.rules if not r.canAnswer(target)]
+        self.targetRules = [r for r in rules if r.canAnswer(target)]
+        self.rules = [r for r in rules if not r.canAnswer(target)]
         random.shuffle(self.targetRules)
         random.shuffle(self.rules)
         self.finalTarget = target
         self.context = {}
         self.asked = 0
+        print(self.targetRules)
+        print(self.rules)
+        print(self.finalTarget)
 
-    def resolveAns(self):
+    def solveAkinator(self):
         for r in self.targetRules:
             val = self.__resolveRule(r)
             if val:
@@ -130,7 +137,7 @@ class Akinator:
                 self.__resolveKeyToContext(key=val)
             else:
                 if val:
-                    _drawRule(rule)
+                    AsciiUtil.drawRule(rule)
                 return status, val
 
     def __resolveKeyToContext(self, key):
@@ -145,13 +152,13 @@ class Akinator:
         # else aks user
         if not val:
             self.asked += 1
-            val = ask(
+            val = AsciiUtil.ask(
                 question=f"Выберите '{key}':",
                 options=list(set(sum([r.options(key)
                                       for r in self.rules
                                       if not r.canAnswer(key)],
                                      start=[]))),
-                postHooks=[_drawContextHook(self.context)])
+                postHooks=[AsciiUtil.drawContextHook(self.context)])
 
         self.__updContext(key, val)
 
@@ -169,7 +176,81 @@ class Akinator:
         return len(self.context) - self.asked
 
 
-    def drawResult(target, ansDict, fallbackOpts):
+class AsciiUtil:
+    def ask(question, options, preHooks=None, postHooks=None):
+        choice = None
+
+        def _do_multiple_choice(stdscr):
+            attributes = {}
+            curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
+            attributes['normal'] = curses.color_pair(1)
+
+            curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
+            attributes['highlighted'] = curses.color_pair(2)
+
+            c = 0  # last character read
+            option = 0  # the current option that is marked
+            while c != 10:  # Enter in ascii
+                stdscr.erase()
+                AsciiUtil.__drawHooks(stdscr, preHooks)
+
+                stdscr.addstr(f"{question}\n", curses.A_UNDERLINE)
+                for i in range(len(options)):
+                    attr = attributes['highlighted'] if i == option else attributes['normal']
+                    stdscr.addstr(f"{i + 1}. ")
+                    stdscr.addstr(options[i] + '\n', attr)
+
+                stdscr.addstr('\n')
+                AsciiUtil.__drawHooks(stdscr, postHooks)
+
+                c = stdscr.getch()
+                if 1 <= c - ord('0') <= len(options):
+                    option = c - ord('0') - 1
+                elif c == curses.KEY_UP and option > 0:
+                    option -= 1
+                elif c == curses.KEY_DOWN and option < len(options) - 1:
+                    option += 1
+
+            nonlocal choice
+            choice = options[option]
+
+        curses.wrapper(_do_multiple_choice)
+        return choice
+
+    def __drawHooks(stdcrs, hooks):
+        if hooks:
+            for h in hooks:
+                h(stdcrs)
+
+    def drawMessageHook(message):
+        def _hook(stdscr):
+            stdscr.addstr(f'{message}\n')
+        return _hook
+
+    def drawContextHook(context):
+        def _hook(stdscr):
+            stdscr.addstr('\nТекущий контекст:\n')
+            for k in context:
+                stdscr.addstr(f'\t{k:20}{context[k]}\n')
+        return _hook
+
+    def drawRule(rule):
+        def _draw(stdscr):
+            stdscr.erase()
+            stdscr.addstr(f"По правилу:\n", curses.A_UNDERLINE)
+            stdscr.addstr(f"если:\n")
+            d = rule.getWhen()
+            for k in d:
+                stdscr.addstr(f'\t{k:20}{d[k]}\n')
+            stdscr.addstr(f"то:\n")
+            d = rule.getThen()
+            for k in d:
+                stdscr.addstr(f'\t{k:20}{d[k]}\n')
+            stdscr.getch()
+
+        curses.wrapper(_draw)
+
+    def drawResult(self, target, ansDict, fallbackOpts, asked, guessed):
         def _draw(stdscr):
             stdscr.erase()
             if ansDict:
@@ -178,46 +259,14 @@ class Akinator:
                 stdscr.addstr(
                     f"\nАкинатор задал {asked} вопрос(ов|а)\nи вывел {guessed} значени(й|я)\n")
             else:
-                ask(question="Что вы загадали?",
-                    options=fallbackOpts,
-                    preHooks=[_drawMessageHook(f"Все известные правила для '{target}' оказались ложью.\n\n")])
+                AsciiUtil.ask(question="Что вы загадали?",
+                              options=fallbackOpts,
+                              preHooks=[AsciiUtil.drawMessageHook(f"Все известные правила для '{target}' оказались ложью.\n\n")])
 
             stdscr.getch()
 
         curses.wrapper(_draw)
 
-
-def _drawContextHook(context):
-    def _hook(stdscr):
-        stdscr.addstr('\nТекущий контекст:\n')
-        for k in context:
-            stdscr.addstr(f'\t{k:20}{context[k]}\n')
-
-    return _hook
-
-
-def _drawMessageHook(message):
-    def _hook(stdscr):
-        stdscr.addstr(f'{message}\n')
-
-    return _hook
-
-
-def _drawRule(rule):
-    def _draw(stdscr):
-        stdscr.erase()
-        stdscr.addstr(f"По правилу:\n", curses.A_UNDERLINE)
-        stdscr.addstr(f"если:\n")
-        d = rule.getWhen()
-        for k in d:
-            stdscr.addstr(f'\t{k:20}{d[k]}\n')
-        stdscr.addstr(f"то:\n")
-        d = rule.getThen()
-        for k in d:
-            stdscr.addstr(f'\t{k:20}{d[k]}\n')
-        stdscr.getch()
-
-    curses.wrapper(_draw)
 
 RuleState = Enum('RuleState', 'FAIL SUCCESS UNKNOWN')
 
@@ -282,52 +331,6 @@ class Rule:
     def targetOptions(self, key):
         v = self.__then.get(key, None)
         return [v] if v else []
-
-
-def ask(question, options, preHooks=None, postHooks=None):
-    choice = ''
-
-    def _do_multiple_choice(stdscr):
-        attributes = {}
-        curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
-        attributes['normal'] = curses.color_pair(1)
-
-        curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
-        attributes['highlighted'] = curses.color_pair(2)
-
-        c = 0  # last character read
-        option = 0  # the current option that is marked
-        while c != 10:  # Enter in ascii
-            stdscr.erase()
-            _drawHooks(stdscr, preHooks)
-
-            stdscr.addstr(f"{question}\n", curses.A_UNDERLINE)
-            for i in range(len(options)):
-                attr = attributes['highlighted'] if i == option else attributes['normal']
-                stdscr.addstr(f"{i + 1}. ")
-                stdscr.addstr(options[i] + '\n', attr)
-
-            _drawHooks(stdscr, postHooks)
-
-            c = stdscr.getch()
-            if 1 <= c - ord('0') <= len(options):
-                option = c - ord('0') - 1
-            elif c == curses.KEY_UP and option > 0:
-                option -= 1
-            elif c == curses.KEY_DOWN and option < len(options) - 1:
-                option += 1
-
-        nonlocal choice
-        choice = options[option]
-
-    curses.wrapper(_do_multiple_choice)
-    return choice
-
-
-def _drawHooks(stdcrs, hooks):
-    if hooks:
-        for h in hooks:
-            h(stdcrs)
 
 
 if __name__ == "__main__":
